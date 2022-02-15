3723b782e8c0b80c5c570c3785778b4b93e98ee9
Mananta <99699041+Raie-Mananta@users.noreply.github.com>
2022-02-15 19:31:25 +0100

    Create Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,90 @@
+INSERT INTO gestResa.Compagnie VALUES('JAL', 'Japan Airlines', 'Japon');
+INSERT INTO gestResa.Compagnie VALUES('AFR', 'Air France', 'France');
+INSERT INTO gestResa.Compagnie VALUES('BAW', 'British Airways', 'Royaume-Uni');
+INSERT INTO gestResa.Compagnie VALUES('CCA', 'china Airlines', 'Chine');
+INSERT INTO gestResa.Compagnie VALUES('DLH', 'Lufthansa', 'Allemagne');
+INSERT INTO gestResa.Compagnie VALUES('PVL', 'PAL Airlines', 'Canada');
+
+INSERT INTO gestResa.Aeroport VALUES(1, 'Aéroport international du Chūbu', 'Tokoname', 'Japon', 'JAL');  
+INSERT INTO gestResa.Aeroport VALUES(2, 'Aéroport international de Narita', 'Tokyo', 'Japon', 'JAL');
+INSERT INTO gestResa.Aeroport VALUES(3, 'Paris-Charles-de-Gaulle', 'Roissy-en-France', 'France', 'AFR');  
+INSERT INTO gestResa.Aeroport VALUES(4, 'Bordeaux-Mérignac', 'Mérignac', 'France', 'AFR');
+INSERT INTO gestResa.Aeroport VALUES(5, 'Aéroport de Londres Heathrow', 'Londres', 'Royaume-Uni', 'BAW');  
+INSERT INTO gestResa.Aeroport VALUES(6, 'Aéroport de Londres Gatwick', 'Londres', 'Royaume-Uni', 'BAW');
+INSERT INTO gestResa.Aeroport VALUES(7, 'Aéroport International de Pékin-Capitale', 'Pékin', 'Chine', 'CCA');  
+INSERT INTO gestResa.Aeroport VALUES(8, 'Aéroport international de Chéngdū-Shuāngliú', 'Chengdu', 'Chine', 'CCA');
+INSERT INTO gestResa.Aeroport VALUES(9, 'Aéroport de Francfort-sur-le-Main', 'Francfort-sur-le-Main', 'Allemagne', 'DLH');  
+INSERT INTO gestResa.Aeroport VALUES(10, 'Aéroport Franz-Josef-Strauß de Munich', 'Munich', 'Allemagne', 'DLH');
+INSERT INTO gestResa.Aeroport VALUES(11, 'Aéroport international de Saint-Jean de Terre-Neuve', 'Saint-Jean de Terre-Neuve', 'Canada', 'PVL');  
+INSERT INTO gestResa.Aeroport VALUES(12, 'Aéroport international Pierre-Elliott-Trudeau de Montréal', 'Montréal', 'Canada', 'PVL');
+
+
+INSERT INTO gestResa.Client VALUES(1, 'Junko', 'Touhou', '1998-01-01', 'Japon', '24 rue du cerisier, Tokyo', '0088 224  800', 'JTouhou@gmail.jp','051254535');
+
+INSERT INTO gestResa.Client VALUES(2, 'Sans', 'Undertale', '2001-08-06', 'Canada', '69 rue du Sirop érable, Montréal', '06 52 58 65 87', 'S.undertale@gmail.ca','584485087');
+
+INSERT INTO gestResa.Client VALUES(3, 'Sora', 'Hearts', '1985-08-06', 'Chine', '42 avenue du Panda, Hong Kong', '05 47 73 54 87', 'sHearts@gmail.ch','841598404');
+
+INSERT INTO gestResa.Client VALUES(4, 'Mario', 'Bros', '1963-08-01', 'Italie', '1 allées des Pizza, Venise', '01 54 24 74 36', 'Mario@gmail.it','484085485');
+
+INSERT INTO gestResa.Client VALUES(5, 'Link', 'Zelda', '1996-04-06', 'Allemand', '23 ruelle des Bières, Berlin', '04 37 80 54 87', 'Zelda@gmail.de','000000001');
+
+INSERT INTO gestResa.Client VALUES(6, 'Hershel', 'Layton', '1976-04-06', 'Royaume-Uni', '12 rue du thé, Londre', '03 34 46 24 88', 'HershelL@gmail.uk','485156874');
+
+INSERT INTO gestResa.Client VALUES(7, 'Bob', 'Lennon', '1986-02-08', 'France', '12 rue de la baguette, Bus', '06 69 87 90 12', 'BobLennon@gmail.fr','48510884');
+
+INSERT INTO gestResa.Client VALUES(8, 'Byakuren', 'Hijiri', '1986-08-02', 'Japon', '54 allée des Sushi, Kyoto', '06 54 10 80 09', 'ByakuHiji@gmail.jp','548157245');
+
+INSERT INTO gestResa.Client VALUES(9, 'Emmy', 'Altava', '1996-09-04', 'Royaume-Uni', '167 avenue des roses, Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch', '06 54 80 09', 'EmAlta@gmail.uk','518472518');
+
+INSERT INTO gestResa.Client VALUES(10, 'Jean', 'Aipa', '1986-01-04', 'France', '41 impasse du vin, Y', '06 53 46 42 58', 'JeanAipa@gmail.fr','518457245');
+
+INSERT INTO gestResa.Client VALUES(11, 'Alain', 'Terieur', '2001-09-21', 'Canada', '42 imposse du vaing, Montréal', '06 53 30 42 58', 'InTerieur@gmail.ca','512457845');
+
+INSERT INTO gestResa.Client VALUES(12, 'Alex', 'Terieur', '2001-09-21', 'Canada', '42 imposse du vaing, Montréal', '06 49 81 56 33', 'ExTerieur@gmail.ca','518457245');
+
+INSERT INTO gestResa.Client VALUES(13, 'Jean', 'Peplu', '2009-02-25', 'France', '20 rue Lecourbe, Paris', '06 53 30 42 58', 'jpp@gmail.ca','515427548');
+
+INSERT INTO gestResa.Client VALUES(14, 'Haruto', 'Satou', '1999-02-12', 'Japon', '26 avenue des Satou, Tokyo', '06 45 30 28 58', 'haruharuman@gmail.jp','512456598');
+
+INSERT INTO gestResa.Client VALUES(15, 'James', 'Bound', '1989-01-01', 'Etats-Unis', '50 rue du Gras, Los Angeles', '06 22 30 65 87', 'jamesthebound@gmail.com','356295683');
+
+INSERT INTO gestResa.Client VALUES(16, 'Hank', 'East', '1969-05-03', 'Royaume-Uni', '15 avenus Fishandchips, Londres', '06 11 25 45 97', 'oibruv@gmail.uk','364273564');
+
+INSERT INTO gestResa.Client VALUES(17, 'Frank', 'West', '1955-04-018', 'Royaume-Uni', '24 rue Pogman, Manchester', '05 34 45 45 64', 'coveredwars@gmail.uk','326594815');
+
+INSERT INTO gestResa.Client VALUES(18, 'Marie', 'Martin', '1957-10-10', 'France', '24 rue Bavette, Marseille', '06 42 48 23 19', 'incroyable@gmail.fr','613265548');
+
+INSERT INTO gestResa.Client VALUES(19, 'Martin', 'Henri', '1984-03-11', 'France', '24 rue Paul, Paris', '06 42 44 42 43', 'gamerman@gmail.fr','642154676');
+
+INSERT INTO gestResa.Client VALUES(20, 'Gordon', 'Freeman', '1973-08-08', 'Etats-Unis', '24 rue Combine, Seattle', '06 24 56 83 77', 'riseandshine@gmail.com','543162548');
+
+INSERT INTO gestResa.Vol VALUES(1, '20:30:00', '2017-12-24', '22:00:00', '2017-12-24', 5, 6, 'BAW', 190);
+INSERT INTO gestResa.Vol VALUES(2, '12:30:00', '2019-10-15', '20:00:00', '2019-10-15', 3, 12, 'AFR', 250);
+INSERT INTO gestResa.Vol VALUES(3, '20:12:00', '2015-05-05', '06:12:00', '2015-05-06', 9, 7, 'DLH', 250);
+INSERT INTO gestResa.Vol VALUES(4, '17:30:00', '2005-02-23', '11:00:00', '2005-02-24', 12, 2, 'PVL', 250);
+INSERT INTO gestResa.Vol VALUES(5, '07:43:00', '2020-03-15', '17:43:00', '2020-03-15', 8, 4, 'CCA', 250);
+
+INSERT INTO gestResa.Reservation VALUES(1, 'Tokyo', '2020-03-05', 'eco', 310, 'res', 1, 1);
+INSERT INTO gestResa.Reservation VALUES(2, 'Montréal', '2019-10-05', 'eco', 323, 'res', 2, 2);
+INSERT INTO gestResa.Reservation VALUES(3, 'Hong Kong', '2015-04-24', 'bui', 1100, 'res', 3, 3);
+INSERT INTO gestResa.Reservation VALUES(4, 'Venise', '2015-04-24', 'eco', 270, 'res', 4, 4);
+INSERT INTO gestResa.Reservation VALUES(5, 'Berlin', '2020-03-01', 'pre', 2200, 'res', 5, 5);
+
+INSERT INTO gestResa.Reservation VALUES(6, 'Tokyo', '2020-03-05', 'bui', 310, 'res', 6, 1);
+INSERT INTO gestResa.Reservation VALUES(7, 'Montréal', '2019-10-05', 'pre', 1100, 'res', 7, 2);
+INSERT INTO gestResa.Reservation VALUES(8, 'Hong Kong', '2015-04-24', 'bui', 2200, 'res', 8, 3);
+INSERT INTO gestResa.Reservation VALUES(9, 'Venise', '2015-04-24', 'bui', 1100, 'res', 9, 4);
+INSERT INTO gestResa.Reservation VALUES(10, 'Berlin', '2020-03-01', 'pre', 2200, 'res', 10, 5);
+
+INSERT INTO gestResa.Reservation VALUES(11, 'Tokyo', '2020-03-05', 'eco', 310, 'res', 11, 1);
+INSERT INTO gestResa.Reservation VALUES(12, 'Montréal', '2019-10-05', 'eco', 323, 'res',2, 2);
+INSERT INTO gestResa.Reservation VALUES(13, 'Hong Kong', '2015-04-24', 'bui', 1100, 'res',13, 3);
+INSERT INTO gestResa.Reservation VALUES(14, 'Venise', '2015-04-24', 'eco', 270, 'res', 14, 4);
+INSERT INTO gestResa.Reservation VALUES(15, 'Berlin', '2020-03-01', 'pre', 2200, 'res',15, 5);
+
+INSERT INTO gestResa.Reservation VALUES(16, 'Tokyo', '2020-03-05', 'bui', 310, 'res', 16, 1);
+INSERT INTO gestResa.Reservation VALUES(17, 'Montréal', '2019-10-05', 'pre', 1100, 'res', 17, 2);
+INSERT INTO gestResa.Reservation VALUES(18, 'Hong Kong', '2015-04-24', 'bui', 2200, 'res', 18, 3);
+INSERT INTO gestResa.Reservation VALUES(19, 'Venise', '2015-04-24', 'bui', 1100, 'res', 19, 4);
+INSERT INTO gestResa.Reservation VALUES(20, 'Berlin', '2020-03-01', 'pre', 2200, 'res', 20, 5);
